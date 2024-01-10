826c199a90d17bee2976e1ff9b211ec3ea0e0dc3
Juliana Guimarães Andrade <106482908+julianagandrade@users.noreply.github.com>
2024-01-10 20:21:31 -0300

    Implementando o smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,13 +1,20 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+    public abstract class Smartphone 
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Numero { get; set; } 
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get;}
+        protected string Imei { get;}
+        protected int Memoria { get;}
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
